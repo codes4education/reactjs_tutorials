24deed2f938d248df4e857b1d6243d88e99c984b
Bhaskar Gupta <codeforeducations@gmail.com>
2023-06-17 12:25:59 +0530

    part-9 Click Event With Function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import "./App.css";
-import User from "./User.jsx";
 
 function App() {
+  // Example 1
+  const demo = () => {
+    alert("Hello Everyone !!");
+  };
+
+  // Example 5
+  let data = "Code4Education";
+  const demo1 = () => {
+    data = "Bhaskar Gupta";
+    alert(data);
+  };
+
   return (
     <>
       <div className="App">
-        <h1>Hello World !!</h1>
+        {/* Example 5 Print your variable data */}
+        <h1>{data}</h1>
 
-        <User />
+        {/* Example 1 onLoad Function Call Automative  */}
+        <button onClick={demo()}>Click Me</button>
+
+        {/* Example 2 When you clicked the button */}
+        <button onClick={demo}>Click Me</button>
+
+        {/* Example 3 Inline Define Alert Box Method */}
+        <button onClick={() => alert("Hello Everyone !!")}>Click Me</button>
+
+        {/* Example 4 Function call with Arrow function Method */}
+        <button onClick={() => demo()}>Click Me</button>
+
+        {/* Example 5 Variables data with Function  */}
+        <button onClick={demo1}>Click Me</button>
       </div>
     </>
   );

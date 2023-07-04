8169673860f282b9c2c95e65ed48eb877e80d530
Bhaskar Gupta <codeforeducations@gmail.com>
2023-07-04 21:46:34 +0530

    form handling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-
+import FormHandling from "./FormHandling";
 function App() {
-  const [status, setStatus] = useState(true);
   return (
     <>
       <div className="App">
-        <h1>Show, Hide & Toggle</h1>
+        <h1>Form Handling</h1>
 
-        {status ? <div className="status">Content</div> : null}
-
-        {/* Example 1 */}
-        <button onClick={() => setStatus(false)}>Hide</button>
-        <button onClick={() => setStatus(true)}>Show</button>
-
-        {/* Example 2 Toggle */}
-        <button onClick={() => setStatus(!status)}>Toggle</button>
+        <FormHandling />
       </div>
     </>
   );

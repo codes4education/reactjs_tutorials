ce984142080c3f9bffba610c9b0b0667f324faef
Bhaskar Gupta <codeforeducations@gmail.com>
2023-07-13 18:35:40 +0530

    Constructor Method Lifecycle

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./App.css";
-import Users from "./Users";
-function App() {
-  // Example 1 Function to be passed as a props
-  const handleButtonClick = () => {
-    console.log("Button clicked!");
-  };
-
-  // Example 2 useState
-  const [count, setCount] = useState(0);
-  const incrementCount = () => {
-    setCount(count + 1);
-  };
-
-  return (
-    <>
-      <div className="App">
-        <h1>Pass Function as Props</h1>
-
-        <Users
-          handleClick={handleButtonClick}
-          count={count}
-          increment={incrementCount}
-        />
-      </div>
-    </>
-  );
+class App extends Component {
+  // Example 1
+  constructor() {
+    super();
+    console.log("constructor method!!");
+  }
+  render() {
+    console.log("render method!!");
+    return (
+      <>
+        <div className="App">
+          <h1>Lifecycle Methods In React.</h1>
+        </div>
+      </>
+    );
+  }
 }
 export default App;

b3e695ac46283394f504a80fe74295c16be50800
Bhaskar Gupta <codeforeducations@gmail.com>
2023-07-22 15:40:08 +0530

    useEffect with condition

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import UserTotal from "./UserTotal";
 function App() {
   // Example 2
   const [count, setCount] = useState(0);
+  const [total, setTotal] = useState(100);
 
-  // Example 1
   useEffect(() => {
-    // alert("useEffect Hook!");
-    console.log("useEffect Hook!");
+    // console.log("After Render Total!!");
+  }, [total]);
 
-    console.log("Updating");
-  });
+  useEffect(() => {
+    // console.log("After Render Count!!");
+  }, [count]);
+
   return (
     <>
       <div className="App">
-        <h1>useEffect Hook</h1>
+        <h1>useEffect With Condition Hook</h1>
+
+        <UserTotal count={count} total={total} />
 
         {/* Example 2 */}
-        <h2>Count: {count}</h2>
+        {/* <h2>Count:{count}</h2> */}
         <button onClick={() => setCount(count + 1)}>Update Count</button>
+
+        {/* Example 3 */}
+        {/* <h2>Total:{total}</h2> */}
+        <button onClick={() => setTotal(total + 1)}>Update Total</button>
       </div>
     </>
   );

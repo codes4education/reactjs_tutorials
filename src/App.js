3f938705308bb6bf747da3d8a2548fcee9b44095
Bhaskar Gupta <codeforeducations@gmail.com>
2023-07-24 20:20:28 +0530

    bootstrap setup

--- src/App.js
+++ src/App.js
@@ -1,45 +1,20 @@
 import React from "react";
 import "./App.css";
 
-// Example 2
-import "./style.css";
-
-// Example 4
-import Style from "./style.module.css";
-
-// Example 5
-import "./style.scss";
+// Bootstrap 5
+import { Button, Alert } from "react-bootstrap";
 
 function App() {
-  // Example 3
-  const headline = {
-    color: "#fff",
-    backgroundColor: "blue",
-  };
-
   return (
     <>
       <div className="App">
-        <h1>Styles Type In React.</h1>
+        <h1>Install Bootstrap In React.</h1>
 
-        {/* Example 1 Inline CSS */}
-        <h1 style={{ color: "#fff", backgroundColor: "green" }}>
-          Inline Method
-        </h1>
+        {/* Button */}
+        <Button variant="primary">Primary</Button>
 
-        {/* Example 2 Normal CSS */}
-        <h1 className="primary">Normal Method</h1>
-
-        {/* Example 3 CSS In JS */}
-        <h1 style={headline}>CSS In JS Method</h1>
-
-        {/* Example 4 CSS Module */}
-        <h1 className={Style.title}>CSS Module Method</h1>
-
-        {/* Example 5 Sass & Scss */}
-        <h1 className="primary-sass">
-          Sass & <span>Scss</span> Method
-        </h1>
+        {/* Alert */}
+        <Alert variant="danger">This is a success alert—check it out!</Alert>
       </div>
     </>
   );

ae56de40e52afb84683176d0d4ee80b518d7f7ae
Bhaskar Gupta <codeforeducations@gmail.com>
2023-07-29 20:16:56 +0530

    fragment tag

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React from "react";
 import "./App.css";
-import ReusableButton from "./ReusableButton";
-import ReusableList from "./ReusableList";
+import Fragment from "./FragmentLayout";
 
 function App() {
-  // Example 1
-  const handleButtonClick = () => {
-    console.log("Button Clicked!");
-  };
-
-  // Example 2
-  const countries = ["USA", "Canada", "India", "UK", "Aust", "Sri"];
-
   return (
-    <>
-      <div className="App">
-        <h1>Reuse Component In React.</h1>
-
-        {/* Example 1 */}
-        <ReusableButton text="Click Me" onClick={handleButtonClick} />
-        <ReusableButton text="Submit" onClick={handleButtonClick} />
-
-        {/* Example 2 */}
-        <ReusableList items={countries} />
-      </div>
-    </>
+    <div className="App">
+      <h1>Fragment In React.</h1>
+      <Fragment />
+    </div>
   );
 }
 export default App;
--- src/ReusableButton.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ReusableButton({ text, onClick }) {
-  return <button onClick={onClick}>{text}</button>;
-}

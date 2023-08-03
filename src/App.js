c8e0b303846b598df13d951511283c78b11814cf
Bhaskar Gupta <codeforeducations@gmail.com>
2023-08-03 14:37:17 +0530

    class component ref

--- src/App.js
+++ src/App.js
@@ -1,52 +1,39 @@
-import React, { useState, useMemo } from "react";
+import React, { Component, createRef } from "react";
 import "./App.css";
 
-function App() {
-  // Example 1
-  const [number1, setNumber1] = useState(0);
-  const [number2, setNumber2] = useState(0);
-  const [count, setCount] = useState(0);
+class App extends Component {
+  constructor() {
+    super();
+    this.myInputRef = createRef();
+  }
 
   // Example 1
-  // const sum = () => {
-  //   console.log("Calculating sum...! ");
-  //   return number1 + number2;
-  // };
+  // componentDidMount() {
+  //   this.myInputRef.current.value = "Bhaskar Gupta";
+  //   console.log(this.myInputRef.current.value);
+  // }
 
   // Example 2
-  const sum = useMemo(() => {
-    console.log("Calculating sum...! ");
-    return number1 + number2;
-  }, [number1, number2]);
+  handleClick = () => {
+    this.myInputRef.current.focus();
+    this.myInputRef.current.style.background = "green";
+    this.myInputRef.current.style.color = "white";
 
-  return (
-    <div className="App">
-      <h1>useMemo In React.</h1>
+    console.log(this.myInputRef.current.value);
+  };
 
-      {/* Example 1 */}
-      <div>
-        <input
-          type="number"
-          value={number1}
-          onChange={(e) => setNumber1(Number(e.target.value))}
-        />
-        <input
-          type="number"
-          value={number2}
-          onChange={(e) => setNumber2(Number(e.target.value))}
-        />
+  render() {
+    // console.log(this.myInputRef);
+    return (
+      <div className="App">
+        <h1>Ref In React.</h1>
 
         {/* Example 1 */}
-        {/* <p>Sum: {sum()}</p> */}
 
-        {/* Example 2 */}
-        <p>Sum: {sum}</p>
-
-        {/* Example 1 */}
-        <h2>Count Number: {count}</h2>
-        <button onClick={() => setCount(count + 1)}>Click ++</button>
+        <input type="text" ref={this.myInputRef} />
+        <button onClick={this.handleClick}>Focus Input</button>
       </div>
-    </div>
-  );
+    );
+  }
 }
 export default App;

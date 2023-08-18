7e05015287a3db94f2fbf8e8592893952a070ad8
Bhaskar Gupta <codeforeducations@gmail.com>
2023-08-18 23:28:55 +0530

    context API

--- src/App.js
+++ src/App.js
@@ -2,23 +2,25 @@ import React, { createContext } from "react";
 import "./App.css";
 import Child1 from "./Child1";
 
-// Create, provider, useContext
+// Create, provider, Consumer
 
 const MyContext = createContext();
+const MyContextNew = createContext();
 
 function App() {
   const sharedData = "Hello from Parent!!";
+  const sharedDataNew = "Multiple Entry!!";
 
   return (
     <div className="App">
       <MyContext.Provider value={sharedData}>
-        <h1>useContext Hook</h1>
-
-        {/* Example 1 */}
-        <Child1 />
+        <MyContextNew.Provider value={sharedDataNew}>
+          <h1>Context API</h1>
+          <Child1 />
+        </MyContextNew.Provider>
       </MyContext.Provider>
     </div>
   );
 }
 export default App;
-export { MyContext };
+export { MyContext, MyContextNew };
--- src/Child2.js
+++ src/Child2.js
@@ -1,13 +1,28 @@
-import React, { useContext } from "react";
-import { MyContext } from "./App";
+import React from "react";
+import { MyContext, MyContextNew } from "./App";
 
 export default function Child2() {
-  const sharedData = useContext(MyContext);
-  console.log(sharedData);
+  // const sharedData = useContext(MyContext);
+  // console.log(sharedData);
   return (
     <>
       <h3>Child2 Component</h3>
-      <p>{sharedData}</p>
+
+      <MyContext.Consumer>
+        {(sharedData) => {
+          return (
+            <MyContextNew.Consumer>
+              {(sharedDataNew) => {
+                return (
+                  <p>
+                    {sharedData} = {sharedDataNew}
+                  </p>
+                );
+              }}
+            </MyContextNew.Consumer>
+          );
+        }}
+      </MyContext.Consumer>
     </>
   );
 }

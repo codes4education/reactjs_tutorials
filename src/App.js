f26e9b2fca29e5afbaf368599e476bdbc9ea70df
Bhaskar Gupta <codeforeducations@gmail.com>
2023-09-26 21:13:04 +0530

    delete method

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
-import UpdateMethod from "./UpdateMethod";
+import DeleteMethod from "./DeleteMethod";
 function App() {
   return (
     <div className="App">
-      <h1>Fetch API Put (Update) Method!</h1>
+      <h1>Fetch API Delete Method!</h1>
 
       {/* Example 1 */}
-      <UpdateMethod />
+      <DeleteMethod />
     </div>
   );
 }
